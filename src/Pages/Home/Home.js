87c8ba66d79120b67a91aa356aadb8a6ef1ab55a
Palash Ahmed <palashbiochem@gmail.com>
2022-10-27 13:45:17 +0600

    Carousel added in Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Carousel1 from '../../assets/Carousel1.jpeg'
+import Carousel2 from '../../assets/Carousel2.jpeg'
+import Carousel3 from '../../assets/Carousel3.jpeg'
+import Carousel4 from '../../assets/Carousel4.jpeg'
 
 const Home = () => {
     return (
-        <div>
-            <h2>this is home</h2>
+        <div style={{background: "#1b5199"}}>
+            <Carousel className='container' fade>
+            <Carousel.Item>
+                <img
+                    className="d-block m-auto my-4 w-100 rounded"
+                    src={Carousel1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block m-auto my-4 w-100 rounded"
+                    src={Carousel2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block m-auto my-4 w-100 rounded"
+                    src={Carousel3}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block m-auto my-4 w-100 rounded"
+                    src={Carousel4}
+                    alt="Forth slide"
+                />
+            </Carousel.Item>
+        </Carousel>
         </div>
     );
 };

28ef6ea40724fcc6782a3ee556fd7e006993feaa
Palash Ahmed <palashbiochem@gmail.com>
2022-10-27 10:37:50 +0600

    Footer Modified

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,51 +1,58 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className='bg-dark mb-0'>
+        <div style={{background: "#1b5199"}}>
             <Container className='text-light pt-4 mt-4'>
                 <Row>
                     <Col lg="3">
                         <h2 className='text-center mb-4'>About Education Unlimited</h2>
-                        <p>Education Unlimited is the country's best skill development solution for students, professionals and freelancers of all ages. Its career track programs and live courses created with experienced industry experts will prepare anyone to build a successful career at home and abroad.</p>
+                        <p className='text-white text-decoration-none fw-semibold'>Education Unlimited is the country's best skill development solution for students, professionals and freelancers of all ages. Its career track programs and live courses created with experienced industry experts will prepare anyone to build a successful career at home and abroad.</p>
 
                     </Col>
                     <Col lg="3">
                         <h2 className='text-center mb-4'>Navigation</h2>
                         <ul>
-                            <li><Link to='/'>Computer E-Learning</Link></li>
-                            <li><Link to='/'>All Courses</Link></li>
-                            <li><Link to='/'>Blog</Link></li>
-                            <li><Link to='/'>Verified Certificate</Link></li>
-                            <li><Link to='/'>Documentation</Link></li>
-                            <li><Link to='/'>FAQ</Link></li>
-                            <li><Link to='/'>Customer Support</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Computer E-Learning</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>All Courses</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>Blog</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>Verified Certificate</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>Documentation</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>FAQ</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold'  to='/'>Customer Support</Link></li>
                         </ul>
                     </Col>
                     <Col lg="3">
                         <h2 className='text-center mb-4'>Work with Us</h2>
                         <ul>
-                            <li><Link to='/'>About Education Unlimited</Link></li>
-                            <li><Link to='/'>Our Team</Link></li>
-                            <li><Link to='/'>Afiliat Program</Link></li>
-                            <li><Link to='/'>Suggest New Course</Link></li>
-                            <li><Link to='/'>Write for Blog</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>About Education Unlimited</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Our Team</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Afiliat Program</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Suggest New Course</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Write for Blog</Link></li>
                         </ul>
                     </Col>
                     <Col lg="3">
                         <h2 className='text-center mb-4'>Career Track</h2>
                         <ul>
-                            <li><Link to='/'>Full Stack Web Development</Link></li>
-                            <li><Link to='/'>Python & JavaScript</Link></li>
-                            <li><Link to='/'>MERN</Link></li>
-                            <li><Link to='/'>Front-end web Development</Link></li>
-                            <li><Link to='/'>Complete Digital Marketing</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Full Stack Web Development</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Python & JavaScript</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>MERN</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Front-end web Development</Link></li>
+                            <li><Link className='text-white text-decoration-none fw-semibold' to='/'>Complete Digital Marketing</Link></li>
                         </ul>
                     </Col>   
                 </Row>
-                <p className='text-center '>Copyright © 2022</p>
+                <div className='text-center'>
+                    <FaFacebook className='fs-3 mx-2'/>
+                    <FaTwitter className='fs-3 mx-2'/>
+                    <FaInstagram className='fs-3 mx-2'/>
+                    <FaYoutube className='fs-3 mx-2'/>
+                </div>
+                <p className='text-center mb-0 py-3 '>Copyright © 2022 Md. Palash Akand</p>
             </Container>
 
         </div>
